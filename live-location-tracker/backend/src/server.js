77534080aaec421e0b6437336865cf6bbd0187ef
malhotrasarika789-blip <malhotrasarika789@gmail.com>
2026-05-02 19:30:11 +0530

    fix server render deployment

--- live-location-tracker/backend/src/server.js
+++ live-location-tracker/backend/src/server.js
@@ -1,5 +1,4 @@
 import dotenv from "dotenv";
-
 dotenv.config();
 
 import express from "express";
@@ -15,62 +14,71 @@ import "./config/passport.js";
 import authRoutes from "./routes/auth.routes.js";
 
 import { connectProducer } from "./producers/locationProducer.js";
-
 import { setupSocket } from "./socket/socketHandler.js";
-
 import { startBroadcastConsumer } from "./consumers/broadcastConsumer.js";
-
 import { startDBConsumer } from "./consumers/dbConsumer.js";
 
 const app = express();
-
 const server = http.createServer(app);
+
+
+const PORT = process.env.PORT || 3000;
+
 
 const io = new Server(server, {
   cors: {
     origin: process.env.FRONTEND_URL,
-    credentials: true
-  }
+    credentials: true,
+  },
 });
 
+
 app.use(cors({
-  origin: process.env.FRONTEND_URL,
-  credentials: true
-}));
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
-app.use(
-  session({
+app.use(session({
     secret: process.env.SESSION_SECRET,
     resave: false,
-    saveUninitialized: false
+    saveUninitialized: false,
   })
 );
 
 app.use(passport.initialize());
-
 app.use(passport.session());
+
 
 app.use("/auth", authRoutes);
 
-await mongoose.connect(
-  process.env.MONGO_URI
-);
+const startServer = async () => {
+  try {
+    console.log("Connecting to MongoDB...");
 
-await connectProducer();
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB Connected");
 
-setupSocket(io);
+    console.log("Connecting Kafka Producer...");
+    await connectProducer();
+    console.log("Kafka Producer Connected");
 
-startBroadcastConsumer(io);
+    setupSocket(io);
 
-startDBConsumer();
+    startBroadcastConsumer(io);
+    startDBConsumer();
 
-server.listen(
-  process.env.PORT,
-  () => {
-    console.log(
-      `Server Running on ${process.env.PORT}`
-    );
-    }
-);
+
+    server.listen(PORT, () => {
+      console.log(`Server Running on port ${PORT}`);
+    });
+
+  } catch (error) {
+    console.error("Server startup failed:", error);
+    process.exit(1);
+  }
+};
+
+startServer();
